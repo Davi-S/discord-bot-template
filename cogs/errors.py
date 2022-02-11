0a4a008ca4bffa006f8f2d37a49166dcdbcbd344
Davi S <67160664+Davi-S@users.noreply.github.com>
2022-02-11 18:45:43 -0300

    exceptions for custom errors
    add different messages for custom errors

--- cogs/errors.py
+++ cogs/errors.py
@@ -1,28 +1,29 @@
 from discord.ext import commands
 
+from excecoes import NotOnWhiteList, OnBlackList
+
 
 class ErrosCog(commands.Cog, name='Errors', description='Catch any errors', command_attrs=dict(hidden=True)):
     def __init__(self, bot):
         self.bot = bot
 
-    async def cog_check(self, ctx):  # check for all commands in this cog
-        return True
-    
-    async def cog_before_invoke(self, ctx:commands.Context):
-        pass
-
-    
-    @commands.Cog.listener()
-    async def on_error(self, event):
-        print(event)
-
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, erro):
-        await ctx.send(f'{ctx.author.mention}, {erro}, {type(erro)}')
+        if isinstance(erro, NotOnWhiteList):
+            return  # NotOnWhiteList error are beeing treated on the developers cog
+
+        if isinstance(erro, OnBlackList):
+            return await ctx.send(f'{ctx.author.mention}, you are on the BlackList and can not '
+                                    'use any bot command.\nPlease contact a developer')
+        
+        await ctx.send(f'{ctx.author.mention}, {erro}')
         
 
 
 def setup(bot):
     bot.add_cog(ErrosCog(bot))
-    print('Erros iniciado')
+    print('Errors initiated')
+
+def teardown(bot):
+    print('Errors unload')
